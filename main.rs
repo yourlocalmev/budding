7f132feb56f9129c72b33b6b9ed03b32b8e5869f
yourlocalmev <githubuser99@outlook.com>
2025-08-12 17:22:26 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -15,6 +15,11 @@ async fn main() -> anyhow::Result<()> {
         env::var("PRIVATE_KEY")?,
         env::var("TOMB_CONTRACT")?.parse::<Address>()?,
     );
+    let default_bps = env::var("DEFAULT_BPS").unwrap_or("10".to_string()).parse::<u64>()?;
+    let large_tx_bps = env::var("LARGE_TX_BPS").unwrap_or("7".to_string()).parse::<u64>()?;
+    let tx_threshold = env::var("TX_THRESHOLD")
+    .unwrap_or("1000000000000000000".to_string()) // 1 ETH in wei
+    .parse::<U256>()?;
     let min_value = ethers::utils::parse_ether(env::var("MIN_ETH_VALUE").unwrap_or("5".into()))?;
     let ws = Arc::new(Provider::<Ws>::connect(&rpc_url).await?);
     let http = Arc::new(Provider::<Http>::try_from(&rpc_url)?);
@@ -33,12 +38,33 @@ async fn main() -> anyhow::Result<()> {
     let mut stream = ws.subscribe_pending_txs().await?;
 
     while let Some(tx_hash) = stream.next().await {
-        let (http, tomb, cache, pool1, pool2, selectors) = 
-            (http.clone(), tomb.clone(), cache.clone(), pool1.clone(), pool2.clone(), selectors.clone());
+        let (http, tomb, cache, pool1, pool2, selectors, default_bps, large_tx_bps, tx_threshold) = 
+            (
+                http.clone(),
+                tomb.clone(),
+                cache.clone(),
+                pool1.clone(),
+                pool2.clone(),
+                selectors.clone(),
+                default_bps,
+                large_tx_bps,
+                tx_threshold,
+            );
 
         tokio::spawn(async move {
             if let Ok(Some(tx)) = http.get_transaction(tx_hash).await {
-                handle_tx(tx, &pool1, &pool2, min_value, &selectors, tomb, cache).await;
+                handle_tx(
+                    tx,
+                    &pool1,
+                    &pool2,
+                    min_value,
+                    &selectors,
+                    tomb,
+                    cache,
+                    default_bps,
+                    large_tx_bps,
+                    tx_threshold,
+                ).await;
             }
         });
     }
@@ -53,14 +79,29 @@ async fn handle_tx(
     selectors: &[&str],
     tomb: Contract<SignerMiddleware<Arc<Provider<Ws>>, LocalWallet>>,
     cache: Arc<Mutex<HashSet<H256>>>,
+    default_bps: u64,
+    large_tx_bps: u64,
+    tx_threshold: U256,
 ) {
-    if tx.to.is_none() || tx.input.0.is_empty() || tx.value < min_value {
+    if tx.to.is_none() {
+        println!("Skipped: tx.to is None");
         return;
     }
+    if tx.input.0.is_empty() {
+        println!("Skipped: tx.input is empty");
+        return;
+    }
+    if tx.value < min_value {
+        println!("Skipped: tx.value below min_value ({})", ethers::utils::format_ether(min_value));
+        return;
+    }
+
     let to = format!("{:?}", tx.to.unwrap()).to_lowercase();
     if to != *pool1 && to != *pool2 {
+        println!("Skipped: tx.to not in target pools");
         return;
     }
+
 
     let calldata = hex::encode(&tx.input.0);
     if !selectors.iter().any(|sel| calldata.starts_with(sel)) {
@@ -70,7 +111,11 @@ async fn handle_tx(
     let signal = build_signal(&tx, pool2, &calldata, &to);
     let sig_hash = H256::from(ethers::utils::keccak256(signal.as_bytes()));
     
-    let royalty_bps = if tx.value > U256::exp10(18) { 500u64 } else { 1000u64 };
+    let royalty_bps = if tx.value > tx_threshold {
+        large_tx_bps
+    } else {
+        default_bps
+    };
 
 
     let mut lock = cache.lock().await;
